71b656268eae3b7ac5cfb0582541af0a0b67b815
dasepi <68197025+dasepi@users.noreply.github.com>
2020-07-14 00:15:45 +0800

    update

--- spider/actions/spider_action.py
+++ spider/actions/spider_action.py
@@ -12,8 +12,8 @@ from datetime import datetime
 
 myclient = pymongo.MongoClient(sys.argv[1])
 mydb = myclient['setu']
-mycol = mydb['setu_all_test']
-mycol_del = mydb['setu_del_test']
+mycol = mydb['setu_all']
+mycol_del = mydb['setu_del']
 username = str(sys.argv[2])
 password = str(sys.argv[3])
 
