547f234f30010506461827a6bec0c989819fa82d
dasepi <68197025+dasepi@users.noreply.github.com>
2020-07-14 01:10:56 +0800

    update

--- spider/spider.py
+++ spider/spider.py
@@ -10,7 +10,7 @@ from retrying import retry
 from datetime import datetime
 
 '''获取配置'''
-with open('/home/ubuntu/download_setu/config.json', 'r', encoding='utf-8') as f:
+with open('./spider/config.json', 'r', encoding='utf-8') as f:
     config = json.loads(f.read())
     print('获取配置成功')
 myclient = pymongo.MongoClient(config['mongodb'])
